a0e753389a45a0e82690fd5fe845741baa85199a
Beni MULUMBA <kimkayna@gmail.com>
2020-02-06 19:21:26 +0100

    Init API with app

--- action-reports/app/controller/InterventionController.php
+++ action-reports/app/controller/InterventionController.php
@@ -20,21 +20,16 @@ class InterventionController extends AppController {
     }
 
     public function interventionsList(){
-        $interventionsList = [
-            [
-                1,
-                "sauvetage feu"
-            ],
-            [
-                2,
-                "sauvetage enfant en danger"
-            ],
-            [
-                3,
-                "sauvetage vielle personne AVC"
-            ]
-        ];
 
-        $this->render('intervention.list', compact('interventionsList'));
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'api/pompier');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($curl);
+
+        var_dump($result);
+
+        $this->render('intervention.list', compact('result'));
+
+        curl_close($curl);
     }
 }
--- action-reports/app/view/intervention/list.php
+++ action-reports/app/view/intervention/list.php
@@ -2,9 +2,11 @@
 
 use App\utils\router\Router;
 
-foreach ($interventionsList as $intervention){
+/*foreach ($interventionsList as $intervention){
     echo '<a href="'. Router::url('test', ['id'=> $intervention[0] ]) .'">
         intervention numéro : '. $intervention[0].'
         nom : '. $intervention[1] .'
         </a><br>';
-}
+}*/
+
+var_dump($result);
